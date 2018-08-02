c700f09ce87825489dfcc13ae1d604bad183e8d4
Kristina Tong <kristina.tong@gmail.com>
2018-08-02 17:04:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,12 @@
 def prime?(int)
-  if int <= 0
+  if int < 2
     false
-  end
-  for i in (2..int-1)
-    if int%i == 0
-      false
+  else
+    for i in (2..int-1)
+      if int%i == 0
+       false
+      end
     end
+   true
   end
-  true
 end
